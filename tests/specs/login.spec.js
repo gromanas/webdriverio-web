c614e054047daffd82ab7be4b4a951a64055084a
gromanas <giorgos.romanas@camelotls.com>
2024-08-27 10:45:37 +0300

    [step-1] - first test

--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -1,10 +1,13 @@
+import { $, browser } from "@wdio/globals";
+
 describe('Login Form', () => {
     it('verify user that can be successfully login', async () => {
+        await browser.url('/');
+        await $('#user-name').setValue('standard_user');
+        await $('#password').setValue('secret_sauce');
+        await $('#login-button').click();
+
+        await $('#shopping_cart_container').isDisplayed();
 
     });
 });
-
-
-
-
-
