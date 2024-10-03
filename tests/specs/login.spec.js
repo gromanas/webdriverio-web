c76499d807a78c2d144f4db3d2a1f53366bf62fb
gromanas <giorgos.romanas@camelotls.com>
2024-10-03 16:37:35 +0300

    [step-6] - invalid scenarios

--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -10,4 +10,30 @@ describe('Login Form', () => {
 
         await expect(await ProductsPage.productsPage).toBeDisplayed();
     });
+
+    it('verify that a locked user cannot login', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login(LOGIN_USERS.LOCKED.username, LOGIN_USERS.LOCKED.password);
+
+        await expect(await LoginPage.hintErrorMsg).toBeDisplayed();
+        await expect(await LoginPage.hintErrorMsg).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+    });
+
+    it('verify that user cannot login if he does not provide any credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login(LOGIN_USERS.NO_USER_DETAILS.username, LOGIN_USERS.NO_USER_DETAILS.password);
+
+        await expect(await LoginPage.hintErrorMsg).toHaveText('Epic sadface: Username is required');
+
+    });
+
+    it('verify that user cannot login if he does not provide any password', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login(LOGIN_USERS.NO_PASSWORD.username, LOGIN_USERS.NO_PASSWORD.password);
+
+        await expect(await LoginPage.hintErrorMsg).toHaveText('Epic sadface: Password is required');
+    });
 });
