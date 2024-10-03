d284a7fd971302c2de94fe8b49816eac3ae34e42
gromanas <giorgos.romanas@camelotls.com>
2024-10-03 16:34:45 +0300

    [step-5] - test constants

---
+++ tests/conf/testConstants.js
@@ -0,0 +1,26 @@
+export const LOGIN_USERS = {
+    LOCKED: {
+        username: 'locked_out_user',
+        password: 'secret_sauce',
+    },
+    NO_MATCH: {
+        username: 'd',
+        password: 'd',
+    },
+    NO_USER_DETAILS: {
+        username: '',
+        password: '',
+    },
+    NO_PASSWORD: {
+        username: 'standard_user',
+        password: '',
+    },
+    PERFORMANCE: {
+        username: 'performance_glitch_user',
+        password: 'secret_sauce',
+    },
+    STANDARD: {
+        username: 'standard_user',
+        password: 'secret_sauce',
+    },
+};
--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -1,11 +1,12 @@
 import LoginPage from "../pages/LoginPage.js";
 import ProductsPage from "../pages/ProductsPage.js";
+import {LOGIN_USERS} from "../conf/testConstants.js";
 
 describe('Login Form', () => {
     it('verify user that can be successfully login', async () => {
         await LoginPage.open();
 
-        await LoginPage.login('standard_user', 'secret_sauce');
+        await LoginPage.login(LOGIN_USERS.STANDARD.username, LOGIN_USERS.STANDARD.password);
 
         await expect(await ProductsPage.productsPage).toBeDisplayed();
     });
