51688e4fb9842e2456c27f2cbf3a14f6a7b3835b
gromanas <giorgos.romanas@camelotls.com>
2024-10-03 16:46:40 +0300

    [step-8] - invalid scenarios combined another way

--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -2,6 +2,12 @@ import LoginPage from "../pages/LoginPage.js";
 import ProductsPage from "../pages/ProductsPage.js";
 import {LOGIN_ERROR_MSG, LOGIN_USERS} from "../conf/testConstants.js";
 
+const testCases = [
+    {username: LOGIN_USERS.LOCKED.username, password: LOGIN_USERS.LOCKED.password, msg: LOGIN_ERROR_MSG.ERROR_MSG_LOCKED},
+    {username: LOGIN_USERS.NO_USER_DETAILS.username, password: LOGIN_USERS.NO_USER_DETAILS.password, msg: LOGIN_ERROR_MSG.ERROR_MSG_NO_USERNAME},
+    {username: LOGIN_USERS.NO_PASSWORD.username, password: LOGIN_USERS.NO_PASSWORD.password, msg: LOGIN_ERROR_MSG.ERROR_MSG_NO_PASSWORD}
+];
+
 describe('Login Form', () => {
     it('verify user that can be successfully login', async () => {
         await LoginPage.open();
@@ -11,20 +17,13 @@ describe('Login Form', () => {
         await expect(await ProductsPage.productsPage).toBeDisplayed();
     });
 
-    it('verify that user cannot login in different cases', async () => {
-        const testCases = [
-            {username: LOGIN_USERS.LOCKED.username, password: LOGIN_USERS.LOCKED.password, msg: LOGIN_ERROR_MSG.ERROR_MSG_LOCKED},
-            {username: LOGIN_USERS.NO_USER_DETAILS.username, password: LOGIN_USERS.NO_USER_DETAILS.password, msg: LOGIN_ERROR_MSG.ERROR_MSG_NO_USERNAME},
-            {username: LOGIN_USERS.NO_PASSWORD.username, password: LOGIN_USERS.NO_PASSWORD.password, msg: LOGIN_ERROR_MSG.ERROR_MSG_NO_PASSWORD}
-        ];
+    testCases.forEach(({username, password, msg}) => {
+        it(`verify that user cannot login in different cases - ${msg}`, async () => {
+                await LoginPage.open();
 
-        for (const test of testCases) {
-            const {username, password, msg} = test;
-            await LoginPage.open();
+                await LoginPage.login(username,password);
 
-            await LoginPage.login(username,password);
-
-            await expect(await LoginPage.hintErrorMsg).toHaveText(msg);
-        }
+                await expect(await LoginPage.hintErrorMsg).toHaveText(msg);
+        });
     });
 });
