964b90b7d453738107c6617a24abc9706581add6
gromanas <giorgos.romanas@camelotls.com>
2024-10-03 15:29:34 +0300

    [step-3] - Add expect

--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -1,11 +1,15 @@
 import { browser } from "@wdio/globals";
 import LoginPage from "../pages/LoginPage.js";
+import ProductsPage from "../pages/ProductsPage.js";
 
 describe('Login Form', () => {
     it('verify user that can be successfully login', async () => {
         await browser.url('/');
+
         await LoginPage.username.setValue('standard_user');
         await LoginPage.password.setValue('secret_sauce');
         await LoginPage.loginBtn.click();
+
+        await expect(await ProductsPage.productsPage).toBeDisplayed();
     });
 });
