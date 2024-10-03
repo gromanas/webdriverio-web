716ad4e7c59aec59ea1788f17143927015f826db
gromanas <giorgos.romanas@camelotls.com>
2024-10-03 16:33:53 +0300

    [step-4] - Methods

--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -1,10 +1,9 @@
-import { browser } from "@wdio/globals";
 import LoginPage from "../pages/LoginPage.js";
 import ProductsPage from "../pages/ProductsPage.js";
 
 describe('Login Form', () => {
     it('verify user that can be successfully login', async () => {
-        await browser.url('/');
+        await LoginPage.open();
 
         await LoginPage.login('standard_user', 'secret_sauce');
 
