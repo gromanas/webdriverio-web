62468daab14ae1829e34e83d83d540afc63c7e3c
gromanas <giorgos.romanas@camelotls.com>
2024-10-03 15:22:31 +0300

    [main] - initial commit

--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -1,13 +1,10 @@
 import { $, browser } from "@wdio/globals";
 
 describe('Login Form', () => {
-    it('verify user that can be successfully login', async () => {
+    it('verify that a user that can be successfully login', async () => {
         await browser.url('/');
         await $('#user-name').setValue('standard_user');
         await $('#password').setValue('secret_sauce');
         await $('#login-button').click();
-
-        await $('#shopping_cart_container').isDisplayed();
-
     });
 });
