08ec2358591c56b6337968be9d5f1127f5a14947
gromanas <giorgos.romanas@camelotls.com>
2024-10-04 13:34:17 +0300

    [step-4] - methods

--- tests/pages/LoginPage.mjs
+++ tests/pages/LoginPage.mjs
@@ -16,6 +16,21 @@ class LoginPage {
     get loginBtn() {
         return $('#login-button');
     }
+
+    get hintErrorMsg() {
+        return $('[data-test="error"]');
+    }
+
+    async open() {
+        await browser.url('/');
+    };
+
+    async login(username, password) {
+        // const {username, password} = userCredentials;
+        await this.username.setValue(username);
+        await this.password.setValue(password);
+        await this.loginBtn.click();
+    };
 }
 
 export default new LoginPage();
--- tests/specs/login.spec.mjs
+++ tests/specs/login.spec.mjs
@@ -1,14 +1,11 @@
-import { browser } from "@wdio/globals";
 import LoginPage from "../pages/LoginPage.mjs";
 import ProductsPage from "../pages/ProductsPage.mjs";
 
 describe('Login Form', () => {
     it('verify user that can be successfully login', async () => {
-        await browser.url('/');
+        await LoginPage.open();
 
-        await LoginPage.username.setValue('standard_user');
-        await LoginPage.password.setValue('secret_sauce');
-        await LoginPage.loginBtn.click();
+        await LoginPage.login('standard_user', 'secret_sauce');
 
         await expect(await ProductsPage.productsPage).toBeDisplayed();
     });
